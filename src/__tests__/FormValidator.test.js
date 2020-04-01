25a51f3d33b09f6b2252ed6ee8e2e074ecf47a4b
Victor Jordan <sistema.sgo.ccb@gmail.com>
2020-04-01 14:09:09 -0300

    Fixed label test

--- src/__tests__/FormValidator.test.js
+++ src/__tests__/FormValidator.test.js
@@ -5,25 +5,25 @@ import {
 } from '../utils/formValidator';
 
 describe('Check e-mail', () => {
-  test('A valid e-mail must return false', () => {
+  test('A valid e-mail must return true', () => {
     expect(validateEmail('victor.jordan@gmail.com')).toBe(true);
   });
-  test('A invalid e-mail must return true', () => {
+  test('A invalid e-mail must return false', () => {
     expect(validateEmail('victor.jorda')).toBe(false);
   });
 });
 
 describe('Check full name', () => {
-  test('A full name with less than 8 characters must return true', () => {
+  test('A full name with less than 8 characters must return false', () => {
     expect(validateFullname('victor')).toBe(false);
   });
-  test('A full name with more than 8 characters must return false', () => {
+  test('A full name with more than 8 characters must return true', () => {
     expect(validateFullname('Victor Jordan')).toBe(true);
   });
 });
 
 describe('Check password', () => {
-  test('Password that meet all requirements must return true', () => {
+  test('Password that meet all requirements must return false', () => {
     expect(validatePassword('teste')).toBe(false);
   });
   test('Password that not meet all requirements must return true', () => {
